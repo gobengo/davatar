27cdc168d67fc5137934f152fc8bccc30f912730
Benjamin Goering <ben@bengo.co>
2022-01-20 00:58:06 -0800

    working prosemirror

--- packages/davatar-storybook/src/stories/RichTextEditor.stories.tsx
+++ packages/davatar-storybook/src/stories/RichTextEditor.stories.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import type { ComponentStory, ComponentMeta } from '@storybook/react';
+import { WebsocketProvider } from 'y-websocket';
+import { WebrtcProvider } from 'y-webrtc';
+import * as Y from "yjs";
+import { prosemirrorToYDoc } from 'y-prosemirror';
 
+const yDoc = new Y.Doc();
+const pmXmlFragment = yDoc.getXmlFragment('prosemirror');
+const provider = new WebsocketProvider('wss://demos.yjs.dev', 'bengo-foo', yDoc);
+const rtcProvider = new WebrtcProvider('bengo-example-document', yDoc);
 import { RichTextEditor } from 'davatar-ui';
 
 const defaultProps: Parameters<typeof RichTextEditor>[0] = {
-  initialText: '(default text)',
+  awareness: provider.awareness,
+  yDoc,
+  type: pmXmlFragment,
 };
 
 export default {
--- packages/davatar-ui/src/RichTextEditor.tsx
+++ packages/davatar-ui/src/RichTextEditor.tsx
@@ -2,12 +2,15 @@ import * as React from "react";
 import {Schema, DOMParser, DOMSerializer} from "prosemirror-model";
 import * as pmExampleSetup from "prosemirror-example-setup";
 // import 'prosemirror-view/style/prosemirror.css';
+import type * as Y from 'yjs';
 
 export type RichText = string;
 
 export const RichTextEditor = function (
     props: {
-        initialText: RichText
+        yDoc: Y.Doc
+        type: Y.XmlFragment
+        awareness: Awareness
     }
 ) {
     return <>
@@ -18,30 +21,40 @@ export const RichTextEditor = function (
     </>;
 };
 
-import {schema} from 'prosemirror-schema-basic';
+import {schema} from './ySchema';
 import {useProseMirror, ProseMirror} from 'use-prosemirror';
 import type { EditorState } from "prosemirror-state";
+import { yCursorPlugin, ySyncPlugin, yUndoPlugin, undo, redo } from "y-prosemirror";
+import type { Awareness } from "y-protocols/awareness";
+import { keymap } from 'prosemirror-keymap';
 
 function ProseMirrorEditor(
     props: {
-        initialText: RichText
+        yDoc: Y.Doc
+        awareness: Awareness
+        type: Y.XmlFragment
     }
 ) {
-    const parser = (content: string) => {
-        const domNode = document.createElement("div");
-        domNode.innerHTML = content;
-        return DOMParser.fromSchema(schema).parse(domNode);
-    };
     const [state, setState] = useProseMirror({
         schema,
-        doc: parser(props.initialText),
         plugins: [
-            ...pmExampleSetup.exampleSetup({ schema, menuBar: false }),
-        ],
+            ySyncPlugin(props.type),
+            yCursorPlugin(props.awareness),
+            yUndoPlugin(),
+            keymap({
+              'Mod-z': undo,
+              'Mod-y': redo,
+              'Mod-Shift-z': redo,
+            }),
+          ].concat(pmExampleSetup.exampleSetup({ schema, menuBar: false })),
     });
     function onProseMirrorChange(s: EditorState) {
         console.log('onProseMirrorChange', s);
         setState(s);
     }
-    return <ProseMirror state={state} onChange={setState} />;
+    return <>
+        <div style={{whiteSpace: 'pre-wrap'}}>
+            <ProseMirror state={state} onChange={setState} />
+        </div>
+    </>;
 };
