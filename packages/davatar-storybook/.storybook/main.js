42bf192a20cbf13b6c81481fcf8338993026e164
Benjamin Goering <ben@bengo.co>
2022-01-28 13:34:53 -0800

    wip

--- packages/davatar-storybook/.storybook/main.js
+++ packages/davatar-storybook/.storybook/main.js
@@ -15,16 +15,6 @@ module.exports = {
     config.resolve.alias.stream = require.resolve("stream-browserify");
     config.resolve.alias.http = require.resolve('stream-http');
     config.resolve.alias.url = require.resolve('rollup-plugin-node-polyfills/polyfills/url');
-    config.build = {
-      ...config.build,
-      rollupOptions: {
-        ...config.build?.rollupOptions,
-        plugins: [
-          ...config.build?.rollupOptions?.plugins || [],
-          require('rollup-plugin-node-polyfills')(),
-        ]
-      }
-    }
     return config;
   }
 }
