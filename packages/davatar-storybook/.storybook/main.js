b5ade7265d62233cb5068aed47e8f14e0c579b7a
Benjamin Goering <ben@bengo.co>
2022-01-28 15:14:41 -0800

    wip

--- packages/davatar-storybook/.storybook/main.js
+++ packages/davatar-storybook/.storybook/main.js
@@ -1,3 +1,10 @@
+const { dependencies } = require("../package.json");
+const { inspect } = require('util');
+const builtins = require('builtin-modules');
+const {default: resolve} = require('@rollup/plugin-node-resolve');
+const commonjs = require('@rollup/plugin-commonjs');
+const analyze = require('rollup-plugin-analyzer')
+
 module.exports = {
   "stories": [
     "../src/**/*.stories.mdx",
@@ -12,16 +19,33 @@ module.exports = {
     "builder": "storybook-builder-vite"
   },
   async viteFinal(config, { configType }) {
+    config.build = {
+      ...config.build,
+      rollupOptions: {
+        ...config.build?.rollupOptions,
+        plugins: [
+          resolve(),
+          commonjs(),
+          ...config.build?.rollupOptions?.plugins || [],
+          analyze(),
+        ],
+        external: builtins,
+        output: {
+          ...config.build?.rollupOptions?.output,
+          // manualChunks: {}
+        }
+      }
+    }
     config.optimizeDeps = {
       ...(config.optimizeDeps || {}),
       exclude: [
         ...(config?.optimizeDeps?.exclude || []),
-        // ""
       ]
     }
     config.resolve.alias.stream = require.resolve("stream-browserify");
     config.resolve.alias.http = require.resolve('stream-http');
     config.resolve.alias.url = require.resolve('rollup-plugin-node-polyfills/polyfills/url');
+    console.log('vite config', inspect(config, false, Infinity));
     return config;
   }
 }
