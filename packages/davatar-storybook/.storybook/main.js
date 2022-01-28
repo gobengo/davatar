b1a8f623b2a294e3c4cf25ad714adea79ed996cb
Benjamin Goering <ben@bengo.co>
2022-01-28 13:21:47 -0800

    dont use vite define global

--- packages/davatar-storybook/.storybook/main.js
+++ packages/davatar-storybook/.storybook/main.js
@@ -15,7 +15,6 @@ module.exports = {
     config.resolve.alias.stream = require.resolve("stream-browserify");
     config.resolve.alias.http = require.resolve('stream-http');
     config.resolve.alias.url = require.resolve('rollup-plugin-node-polyfills/polyfills/url');
-    config.define.global = 'globalThis';
     config.build = {
       ...config.build,
       rollupOptions: {
@@ -26,7 +25,6 @@ module.exports = {
         ]
       }
     }
-    console.log('vite config', JSON.stringify(config, null, 2))
     return config;
   }
 }
